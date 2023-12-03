18e1c9f896edd12b1b949ffa6fcdc6197d08e623
Ediison32 <edisonalvarez32@gmail.com>
2023-12-03 17:44:43 -0500

    localstore

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Vimeo from '@vimeo/player'
+import throttle from 'lodash.throttle'
 
+const videoInfo = document.getElementById('vimeo-player');
+const playVimeoOk = new Vimeo(videoInfo);
+
+
+const saveTime = throttle(function () {
+    playVimeoOk.getCurrentTime().then(function (time) {
+      localStorage.setItem('videoplayer-current-time', time);
+      console.log(localStorage);
+    });
+  }, 1000);
+
+playVimeoOk.on('timeupdate', saveTime);
+
+const timeFinal = localStorage.getItem('videoplayer-current-time');
+console.log("::",timeFinal);
+
+if (timeFinal) {
+    playVimeoOk.setCurrentTime(parseFloat(timeFinal));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
 
+import throttle from "lodash.throttle";
+
+const form = document.querySelector(".feedback-form");
+const email=document.querySelector('input[name="email"]');
+const message= document.querySelector('textarea[name="message"]');
+
+// cargar los datos existentes 
+
+window.addEventListener("load", ()=>{
+
+   const loady =JSON.parse(localStorage.getItem("feedback-form-state"));
+   email.value = loady.email;
+   message.value = loady.message;
+})
+ 
+
+
+form.addEventListener("submit", (event) =>{
+        
+        console.log("entro ");
+        event.preventDefault();
+        console.log(email.value + ":"+ message.value);
+        localStorage.removeItem("feedback-form-state");
+        email.value = "";
+        message.value = "";
+   
+})
+
+form.addEventListener("input", (event)=>{
+        throttle(function (){
+
+                const mensaje = {
+                        "email": email.value,
+                        "message": message.value
+                };
+               // console.log(mensaje);
+                localStorage.setItem("feedback-form-state", JSON.stringify(mensaje))
+                
+        }, 500);
+});
+
