e6bafc398603fa87cbe4066db19722d96b18ac8b
Ediison32 <edisonalvarez32@gmail.com>
2023-12-04 01:34:59 -0500

    localstore arreglo en metodo thorottle

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,33 +10,39 @@ const message= document.querySelector('textarea[name="message"]');
 window.addEventListener("load", ()=>{
 
    const loady =JSON.parse(localStorage.getItem("feedback-form-state"));
-   email.value = loady.email;
-   message.value = loady.message;
-})
- 
 
+   if (loady != ""){
+
+        email.value = loady.email;
+        message.value = loady.message;
+   }
+   
+});
+ 
+const throteleSubmit =throttle(() =>{
+                
+        console.log(email.value + ":"+ message.value);
+        localStorage.removeItem("feedback-form-state");
+        email.value = "";
+        message.value = "";
+}, 500);
 
 form.addEventListener("submit", (event) =>{
         
         console.log("entro ");
         event.preventDefault();
-        console.log(email.value + ":"+ message.value);
-        localStorage.removeItem("feedback-form-state");
-        email.value = "";
-        message.value = "";
+        throteleSubmit();
    
-})
+});
 
 form.addEventListener("input", (event)=>{
-        throttle(function (){
-
-                const mensaje = {
-                        "email": email.value,
-                        "message": message.value
-                };
-               // console.log(mensaje);
-                localStorage.setItem("feedback-form-state", JSON.stringify(mensaje))
-                
-        }, 500);
+        
+        const mensaje = {
+                "email": email.value,
+                "message": message.value
+        };
+        // console.log(mensaje);
+        localStorage.setItem("feedback-form-state", JSON.stringify(mensaje))
+        
 });
 
